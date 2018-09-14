d0b7651734f5b93a32a6e83b8ee479c5904b440d
Juan Letamendia <juan.letamendia@fundkers.com>
2018-09-13 23:04:02 -0300

    Evolution of GDP visualization chart added

--- d3-gdp-evolution/js/main.js
+++ d3-gdp-evolution/js/main.js
@@ -63,6 +63,29 @@ g.append("g")
     .attr("class", "y axis")
     .call(yAxisCall);
 
+    var continents = ["europe","asia","americas","africa"];
+
+    var legend = g.append("g")
+          .attr("transform","translate(" + (width - 10) +"," + (height - 125) + ")");
+
+    continents.forEach(function(continent,i){
+          var legendRow = legend.append("g")
+                       .attr("transform","translate(0, " + (i * 20) + ")");
+               
+              legendRow.append("rect")
+                       .attr("width",10)
+                       .attr("height", 10)
+                       .attr("fill", continentColor(continent));
+                       
+              legendRow.append("text")
+                      .attr("x", -10)
+                      .attr("y", 10)
+                      .attr("text-anchor","end")
+                      .style("text-transform","capitalize")
+                      .text(continent);         
+
+    });      
+
 d3.json("data/data.json").then(function(data){
     console.log(data);
 
